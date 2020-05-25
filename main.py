3238b60459f55f7627ce3afd10eb3e52808c3a12
odhran-o-d <39832722+odhran-o-d@users.noreply.github.com>
2020-05-25 15:40:15 +0100

    made main.py complient with system

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ from utils.evaluation_metrics import SRR, auprc_auroc_ap
 from utils.path_manage import get_files
 
 import torch
-import numpy as np
 from sklearn.utils import shuffle
 import argparse
+from sklearn.model_selection import train_test_split
 
-
-def main(model, optimiser, test_data, epochs, batches):
+def main(model, optimiser, train_data, val_data, epochs, batches):
     mean_reciporical_rank = 0
     break_condition = False
     for epoch in range(epochs):
         # training stage
         model.train()
-        objects, subjects, relationships = load_data(test_data, batches)
+        objects, subjects, relationships = load_data(train_data, batches)
 
         for index in range(batches):
 
@@ -37,22 +36,21 @@ def main(model, optimiser, test_data, epochs, batches):
         if epoch % 10 == 0:
 
             model.eval()
-            objects, subjects, relationships = load_data(test_data, batches)
+            objects, subjects, relationships = load_data(val_data, batch_number=1)
             total_sum_reciporical_rank = torch.zeros(1)
 
-            for index in range(batches):
+            obj = torch.squeeze(torch.LongTensor(objects)).unsqueeze(1)
+            rel = torch.squeeze(torch.LongTensor(relationships)).unsqueeze(1)
+            targets = torch.squeeze(torch.LongTensor(subjects)).unsqueeze(1)
 
-                obj = torch.LongTensor(objects[index])
-                rel = torch.LongTensor(relationships[index])
-                targets = torch.LongTensor(subjects[index])
-
-                predictions = model.forward(e1=obj, rel=rel)
-                srr = SRR(predictions, targets)
-                total_sum_reciporical_rank = total_sum_reciporical_rank + srr
-
+            predictions = model.forward(e1 = obj, rel = rel)
+            srr = SRR(predictions, targets)
+            total_sum_reciporical_rank = total_sum_reciporical_rank + srr
             print("mean reciporical rank is...")
-            print(total_sum_reciporical_rank / len(test_data))
-            MRR = total_sum_reciporical_rank / len(test_data)
+
+
+            print(total_sum_reciporical_rank / len(val_data))
+            MRR = total_sum_reciporical_rank / len(val_data)
             if MRR < (mean_reciporical_rank * 1.05):
                 torch.save(
                     model,
@@ -89,8 +87,8 @@ if __name__ == "__main__":
         type=int,
         help="this is the dimensionality of the embeddings",
     )
-    parser.add_argument("--epochs", default=20, type=int)
-    parser.add_argument("--batches", default=5, type=int)
+    parser.add_argument("--epochs", default=100, type=int)
+    parser.add_argument("--batches", default=100, type=int)
     parser.add_argument("--lr", default=0.01, type=float)
 
     args = parser.parse_args()
@@ -101,10 +99,12 @@ if __name__ == "__main__":
     data, lookup, ASD_dictionary, BCE_dictionary, _, __ = get_files()
     entities = int(len(lookup) / 2)
 
-    x = shuffle(data)
-    test_data = x[:50]
+    # data = shuffle(data)
+    test_data = data[:]
 
-    # split data here into train, eval, test? Make sure eval fits into memory for AUROC?
+    X_train, X_test = train_test_split(test_data, test_size=0.1, random_state=1)
+    X_train, X_val = train_test_split(X_train, test_size=0.1111, random_state=1)
+
 
     if args.model == "DistMult":
         model = DistMult(
@@ -133,7 +133,8 @@ if __name__ == "__main__":
     main(
         model=model,
         optimiser=optimiser,
-        test_data=test_data,
+        train_data=X_train,
+        val_data=X_val,
         epochs=args.epochs,
         batches=args.batches,
     )
