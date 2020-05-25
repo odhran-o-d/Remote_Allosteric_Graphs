c0fbe2a1ef21b6bc27483faa39ab14bc97c12238
odhran-o-d <39832722+odhran-o-d@users.noreply.github.com>
2020-05-25 21:34:18 +0100

    fixed main to work on vm

--- main.py
+++ main.py
@@ -6,25 +6,29 @@ from utils.loaders import load_data, get_onehots
 from utils.evaluation_metrics import SRR, auprc_auroc_ap
 from utils.path_manage import get_files
 
+from tqdm import tqdm
+
 import torch
 from sklearn.utils import shuffle
 import argparse
 from sklearn.model_selection import train_test_split
 
+# @profile
 def main(model, optimiser, train_data, val_data, epochs, batches):
     mean_reciporical_rank = 0
     model_count = 0
     break_condition = False
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs)):
         # training stage
+        model.cuda()
         model.train()
         objects, subjects, relationships = load_data(train_data, batches)
 
         for index in range(batches):
 
-            obj = torch.LongTensor(objects[index])
-            rel = torch.LongTensor(relationships[index])
-            subj = torch.squeeze(torch.LongTensor(subjects[index]))
+            obj = torch.LongTensor(objects[index]).cuda()
+            rel = torch.LongTensor(relationships[index]).cuda()
+            subj = torch.squeeze(torch.LongTensor(subjects[index])).cuda()
 
             optimiser.zero_grad()
             pred = model.forward(e1=obj, rel=rel)
@@ -34,34 +38,26 @@ def main(model, optimiser, train_data, val_data, epochs, batches):
 
         # evaluation stage
 
-        if epoch % 10 == 0:
+        if epoch % 10 == 9:
 
+            model.cpu()
             model.eval()
+
             objects, subjects, relationships = load_data(val_data, batch_number=1)
             total_sum_reciporical_rank = torch.zeros(1)
 
-            obj = torch.squeeze(torch.LongTensor(objects)).unsqueeze(1)
-            rel = torch.squeeze(torch.LongTensor(relationships)).unsqueeze(1)
-            targets = torch.squeeze(torch.LongTensor(subjects)).unsqueeze(1)
+            cpu_obj = torch.squeeze(torch.LongTensor(objects)).unsqueeze(1)#.cuda()
+            cpu_rel = torch.squeeze(torch.LongTensor(relationships)).unsqueeze(1)#.cuda()
+            cpu_subj = torch.squeeze(torch.LongTensor(subjects)).unsqueeze(1)#.cuda()
 
-            predictions = model.forward(e1 = obj, rel = rel)
-            srr = SRR(predictions, targets)
+            predictions = model.forward(e1 = cpu_obj, rel = cpu_rel)
+            srr = SRR(predictions, cpu_subj)
             total_sum_reciporical_rank = total_sum_reciporical_rank + srr
             print("mean reciporical rank is...")
 
-
             print(total_sum_reciporical_rank / len(val_data))
             MRR = total_sum_reciporical_rank / len(val_data)
-            # if MRR < (mean_reciporical_rank * 1.05):
-            #     torch.save(
-            #         model,
-            #         "Model_{model}_Epoch_{epoch}_MRR_{MRR}.pickle".format(
-            #             model=args.model, epoch=epoch, MRR=MRR
-            #         ),
-            #     )
-            #     break_condition = True
-            # else:
-            #     mean_reciporical_rank = MRR
+
             if MRR > mean_reciporical_rank:
                 mean_reciporical_rank = MRR
                 model_count = 0
@@ -104,14 +100,14 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--embdim",
-        default=100,
+        default=1024,
         choices=[100, 256, 625, 1024, 1600, 2025],
         type=int,
         help="this is the dimensionality of the embeddings",
     )
-    parser.add_argument("--epochs", default=400, type=int)
+    parser.add_argument("--epochs", default=100, type=int)
     parser.add_argument("--batches", default=200, type=int)
-    parser.add_argument("--lr", default=0.01, type=float)
+    parser.add_argument("--lr", default=0.0001, type=float)
 
     args = parser.parse_args()
     print(args)
@@ -149,7 +145,7 @@ if __name__ == "__main__":
             num_entities=entities,
             num_relations=4,
         )
-
+    model.cuda()
     optimiser = torch.optim.Adam(model.parameters(), lr=args.lr)
 
     main(
